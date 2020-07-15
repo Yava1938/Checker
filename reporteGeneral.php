012617c71a26cd1bf862b27f7f515d4629cd9319
Rodrigo Yael <yava1938@outlook.com>
2020-07-14 19:00:29 -0500

    Salto de linea reporte

--- reporteGeneral.php
+++ reporteGeneral.php
@@ -1,90 +1,87 @@
 <?php
-require('fpdf182/fpdf.php');
-
+//include pdf_mc_table.php, not fpdf17/fpdf.php
+include('pdf_mc_table.php');
+include('conexion.php');
 session_start();
-if (isset($_SESSION['docente'])) {
-    
 
+//make new object
+$pdf = new PDF_MC_Table();
 
-
-class PDF extends FPDF
-{
-// Cabecera de página
-
-
-
-function Header()
-{
-   //$this->Image('imges/uv.png','0','80','210','100','png');
-    // Arial bold 15
-    $this->SetFont('Arial','B',15);
-    // Movernos a la derecha
-    $this->Cell(80);
-    // Título
-    $this->Cell(30,10,utf8_decode('Reporte General'),0,0,'C');
-    // Salto de línea
-    $this->Ln(20);
-
-    $this->SetFont('Arial','',15);
-
-    $this->cell(10, 10, 'Id', 1, 0, 'C', 0);
-    $this->cell(35, 10, 'Alumno', 1, 0, 'C', 0);
-    $this->cell(35, 10, 'Descripcion', 1, 0, 'C', 0);
-    $this->cell(40, 10, 'Fecha', 1, 0, 'C', 0);
-    $this->cell(35, 10, utf8_decode('Ubicación '), 1, 0, 'C', 0);
-    $this->cell(25, 10, 'Estado', 1, 1, 'C', 0);
-}
-
-// Pie de página
-function Footer()
-{
-    // Posición: a 1,5 cm del final
-    $this->SetY(-15);
-    // Arial italic 8
-    $this->SetFont('Arial','I',8);
-    // Número de página
-    $this->Cell(0,10,utf8_decode('Página ').$this->PageNo().'/{nb}',0,0,'C');
-}
-}
-require 'conexion.php';
-/*$sql = "SELECT a.id_actividad, a.id_alumno, e.nombre_estudiante, a.descripcion_actividad, a.fecha_actividad, a.prioridad, u.nombre_ubicacion
-FROM actividades a INNER JOIN ubicaciones u 
-    ON a.id_ubicacion = u.id_ubicacion
-        INNER JOIN estudiante e
-            ON a.id_alumno = e.id_alumno";*/
-
-$docente = $_SESSION['docente']['Id_Docente'];
-$sql ="SELECT ac.Id_Actividad, al.Nombre_Alumno, ac.Descripcion_Actividad, ac.Fecha_Actividad, u.Nombre_Ubicacion, ac.Estado_Actividad FROM Actividad ac, Alumno al, Ubicacion u WHERE ac.Id_Alumno = al.Id_Alumno AND ac.Id_Ubicacion = u.Id_Ubicacion  AND ac.Id_Docente = '$docente'";            
-
-/*$sql ="Select Id_Pedido, I.Descripcion_Producto,Cantidad_Producto, Precio_Pedido, Cliente_Pedido, Estado_Pedido, Token_Pedido, Fecha_Pedido
-From Inventario I, Pedido c 
-WHERE (I.Id_Producto = c.Id_Producto) AND c.Estado_Pedido = 'Cancelada'"; */
-$res = $conexion_BD->query($sql);
-
-
-
-$pdf = new PDF();
-$pdf->AliasNbPages();
+//add page, set font
 $pdf->AddPage();
+
+$pdf->SetFont('Arial','B',12);
+    // Movernos a la derecha
+    $pdf->Cell(80);
+    // Título
+    $pdf->Cell(30,10,utf8_decode('Reporte de Actividades Realizadas'),0,0,'C');
+    //Dibujo de la línea
+    $pdf->Line(30,20,175,20);
+
+    // Salto de línea
+    $pdf->Ln(20);
+
+//set width for each column (6 columns)
+$pdf->SetWidths(Array(10,40,40,30,25,30));
+
+//set alignment
+$pdf->SetAligns(Array('C','','','C','C','C'));
+
+//set line height. This is the height of each lines, not rows.
+$pdf->SetLineHeight(10);
+
+//add table heading using standard cells
+//set font to bold
+$pdf->SetFont('Arial','B',14);
+$pdf->Cell(10,8,"#",1,0);
+$pdf->Cell(40,8,"Responsable",1,0);
+$pdf->Cell(40,8,utf8_decode('Descripción'),1,0);
+$pdf->Cell(30,8,"Fecha",1,0);
+$pdf->Cell(25,8,"Prioridad",1,0);
+$pdf->Cell(30,8,utf8_decode('Ubicación'),1,0);
+
+
+$pdf->Ln();
+
+//reset font
 $pdf->SetFont('Arial','',10);
 
+//Obtiene los valores de la BD.
+$docente = $_SESSION['docente']['Id_Docente'];
+$sql ="SELECT ac.Id_Actividad, al.Nombre_Alumno, ac.Descripcion_Actividad, ac.Fecha_Actividad, u.Nombre_Ubicacion, ac.Estado_Actividad FROM Actividad ac, Alumno al, Ubicacion u WHERE ac.Id_Alumno = al.Id_Alumno AND ac.Id_Ubicacion = u.Id_Ubicacion  AND ac.Id_Docente = '$docente'";            
+$res = mysqli_query($conexion_BD, $sql);
 
-while ($row =  mysqli_fetch_array($res))
+while ($arrayActividad = mysqli_fetch_array($res))
 {
-    $pdf->cell(10, 10, $row['Id_Actividad'], 1, 0, 'C', 0);
-    $pdf->cell(35, 10, $row['Nombre_Alumno'], 1, 0, 'C', 0);
-    $pdf->cell(35, 10, $row['Descripcion_Actividad'], 1, 0, 'C', 0);
-    $pdf->cell(40, 10, $row['Fecha_Actividad'], 1, 0, 'C', 0);
-    $pdf->cell(35, 10, $row['Nombre_Ubicacion'], 1, 0, 'C', 0);
-    $pdf->cell(25, 10, $row['Estado_Actividad'], 1, 1, 'C', 0);
-    
+  if ($arrayActividad['Estado_Actividad'] == 0) {
+    $estado = "Pendiente";
+  }elseif ($arrayActividad['Estado_Actividad'] == 1) {
+    $estado = "En curso";
+  }elseif ($arrayActividad['Estado_Actividad'] == 2) {
+    $estado = "Pausa";
+  }else{
+    $estado = utf8_decode("Revisión");
+  }
 
+  $descripcion = $arrayActividad['Descripcion_Actividad'];
+  $tamañoDescripcion = strlen($descripcion);
+  $pdf->Row(Array(
+    $arrayActividad['Id_Actividad'],
+    utf8_decode($arrayActividad['Nombre_Alumno']),
+    utf8_decode($arrayActividad['Descripcion_Actividad']),
+    $arrayActividad['Fecha_Actividad'],
+    $estado,
+    utf8_decode($arrayActividad['Nombre_Ubicacion']),
+));
 
 }
 
+
+//output the pdf
 $pdf->Output();
 
-}else{
-    header("location: index.php");
-}
+
+
+
+
 
